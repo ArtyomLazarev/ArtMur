4eb4ca6143c60c1514e12d8fc88cd04d629fadc7
ArtyomLazarev <56193210+ArtyomLazarev@users.noreply.github.com>
2024-02-01 16:08:57 +0300

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,43 @@
+#1
+print ("1) ",'5 + 3 =', 5 + 3 )
+
+#2
+print("2) ","8 % 3 =", 8 % 3)
+
+#3
+print ("3) ", "Python" " " "Programming")
+
+#4
+print ("4) ", '7**3=', 7**3)
+
+#5
+print ("5) ", len("Hello, World!"))
+
+#6
+print ("6) ","Data" "Science")
+
+#7
+print ("7) ", '15//2=', 15//2)
+
+#8
+print ("8) ", '7**3=', 7**3)
+
+#9
+print ("9) ", '-------------------')
+
+#10
+print ("10) "); 
+string1 = "Year:"
+string2 = "2024!"
+string3 = string1 + " " + string2
+print(string3)
+
+print ("Hard", '_____________________________________________________')
+
+
+#1
+a = int(input('a= '))
+b = float(input('b= '))
+c = int(input('c= '))
+d = int(input('d= '))
+print('((a**3 + b**2 - c) / d)', ((a**3 + b**2 - c) / d))
